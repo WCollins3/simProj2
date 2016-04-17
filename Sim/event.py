cc10ba771c69f35bb483c8c61231eece98ddaf51
Alec Gerhart <alecgerhart@Alecs-MacBook-Pro.local>
2016-04-17 16:53:36 -0400

    lots o' changes

--- Sim/event.py
+++ Sim/event.py
@@ -1,12 +1,16 @@
 
 class Event:
 
-    def __init__(self, type, time):
+    def __init__(self, type, time, id):
         self.type = type
         self.time = time
+        self.agent_id = id
 
     def getType(self):
         return self.type
 
     def getTime(self):
         return self.time
+
+    def getAgentID(self):
+        return self.agent_id
